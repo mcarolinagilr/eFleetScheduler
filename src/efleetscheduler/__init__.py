3462ce42cd2e49dfb67e26d83cbed6ed316be4d4
Maria Carolina Gil Ribeiro <mcgr2@UG.KTH.SE>
2025-08-27 13:48:33 +0200

    Prepare 1.0.2

--- src/efleetscheduler/__init__.py
+++ src/efleetscheduler/__init__.py
@@ -11,7 +11,7 @@ __author__ = "Carolina Gil Ribeiro"
 try:
     __version__ = _pkg_version("efleetscheduler")
 except Exception:  # during editable installs/builds
-    __version__ = "1.0.1"
+    __version__ = "1.0.2"
 
 # We export two *modules* and two *symbols*
 __all__ = [
